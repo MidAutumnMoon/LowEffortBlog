a4b7251019c577a9a73b3b4e7bcd69672d195664
MidAutumnMoon <me@418.im>
2024-08-17 19:04:54 +0800

    table of content support for mdx

--- _plugins/RehypeHeadingToc.ts
+++ _plugins/RehypeHeadingToc.ts
@@ -6,59 +6,170 @@
 
 import * as hast from "npm:@types/hast"
 
-import { heading as Heading } from "npm:hast-util-heading"
+import { h } from 'npm:hastscript'
+import { heading as IsHeading } from "npm:hast-util-heading"
 import { headingRank as HeadingRank } from "npm:hast-util-heading-rank"
-import { fromText as HastFromText } from "npm:hast-util-from-text"
+import { toText as HastToText } from "npm:hast-util-to-text"
 
-import { visit as Visit, SKIP, visit } from "npm:unist-util-visit"
+import { fromMarkdown as FromMarkdown } from "npm:mdast-util-from-markdown"
+
+import { visit as Visit, SKIP } from "npm:unist-util-visit"
 import { inspect } from "npm:unist-util-inspect"
 
 
 const Options = {
-    AnchorClassName: "header-anchor"
+    AnchorClassName: "header-anchor",
+    AnchorSymbol: "#",
+    TocClassName: "table-of-content",
 } as const
 
 
 export default function() {
     return function( tree: hast.Root ) {
-        Visit( tree, "element", add_heading_link )
+        /**
+         * N.B. generate_toc before add_heading_link,
+         * otherwise the `Options.AnchorSymbol` will be in the title
+         */
+        generate_toc( tree )
+        add_heading_link( tree )
     }
 }
 
 
-function add_heading_link( node: hast.Element ) {
+function is_valid_heading( node: hast.Element ): boolean {
     /**
      * 1) Skip non heading elements
      */
-    if ( !Heading( node ) ) {
-        return SKIP
+    if ( !IsHeading( node ) ) {
+        return false
     }
-
     /**
      * 2) Skip headings without id,
-     * use rehype-slug to add slugs
+     * use rehype-slug to add slug as id
      */
     if ( !node.properties.id ) {
-        return SKIP
+        return false
     }
-
-    const anchor_link: hast.Element = {
-        type: "element",
-        tagName: "a",
-        properties: {
-            className: Options.AnchorClassName,
-            href: `#${node.properties.id}`,
-            ariaHidden: "true",
-            tabIndex: "-1",
-        },
-        children: [
-            { type: "text", value: "#" } satisfies hast.Text
-        ]
-    }
-
-    node.children = [
-        ...node.children,
-        anchor_link
-    ]
+    return true
 }
 
+function text( value: string ): hast.Text {
+    return { type: "text", value }
+}
+
+
+/**
+ * Add an anchor after each heading linking to the heading itself
+ */
+function add_heading_link( tree: hast.Root ) {
+    Visit( tree, "element", ( node ) => {
+        if ( !is_valid_heading( node ) ) {
+            return SKIP
+        }
+        const anchor_link: hast.Element = {
+            type: "element",
+            tagName: "a",
+            properties: {
+                href: `#${node.properties.id}`,
+                className: Options.AnchorClassName,
+                ariaHidden: "true",
+                tabIndex: "-1",
+            },
+            children: [ text( Options.AnchorSymbol ) ]
+        }
+        node.children.push( anchor_link )
+    } )
+}
+
+
+function generate_toc( tree: hast.Root ) {
+
+    type Heading = {
+        depth: number,
+        text: string,
+        id: string,
+        children: Heading[],
+    }
+
+    const headings: Heading[] = ( () => {
+        const flat_stack: Heading[] = []
+
+        /**
+         * 1) collect headings into a flat array
+         */
+        Visit( tree, "element", ( node ) => {
+            if ( !is_valid_heading( node ) ) {
+                return SKIP
+            }
+
+            /**
+             * Checked for heading before so it is
+             * a heading and will have a rank
+             */
+            const depth = HeadingRank( node )!
+            const text = HastToText( node )
+            const id = `${node.properties.id}`
+
+            flat_stack.push( { depth, text, id, children: [] } )
+        } )
+
+
+        /**
+         * 2) nest headings according to depth
+         *
+         * Ref: https://github.com/lumeland/markdown-plugins/blob/main/toc/mod.ts
+         */
+        
+        const node = { depth: 0, text: "", id: "", children: [] }
+        const stack: Heading[] = [ node ]
+
+        for ( const head of flat_stack ) {
+            if ( head.depth > stack[0].depth ) {
+                stack[0].children.push( head )
+                stack.unshift( head )
+                continue
+            }
+
+            if ( head.depth === stack[0].depth ) {
+                stack[1].children.push( head )
+                stack[0] = head
+                continue
+            }
+
+            while ( head.depth <= stack[0].depth ) {
+                stack.shift()
+            }
+
+            stack[0].children.push( head )
+            stack.unshift( head )
+        }
+
+        return node.children
+    } )()
+
+    const toc = ( () => {
+
+        function all( headings: Heading[] ): hast.Element {
+            return h( "ul", headings.map( one ) )
+        }
+
+        function one( head: Heading ): hast.Element {
+            const link = h( "a",
+                { href: `#${head.id}` },
+                [ head.text ]
+            )
+            return h( "li",
+                [ link, all( head.children ) ]
+            )
+        }
+
+        return h( "section",
+            { class: Options.TocClassName },
+            all( headings )
+        )
+
+    } )()
+
+    tree.children.unshift( toc )
+
+}
