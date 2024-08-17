2f7ca49c8f9de4a3265b7857550391c920e28e90
MidAutumnMoon <me@418.im>
2024-08-17 11:22:33 +0800

    support footnote

--- mdx.config.ts
+++ mdx.config.ts
@@ -1,3 +1,11 @@
 import * as lume_mdx from "lume/plugins/mdx.ts"
 
-export default {} satisfies lume_mdx.Options
+export default {
+
+    rehypeOptions: {
+        allowDangerousHtml: true,
+        // U+EF0E: https://mts.io/2015/04/21/unicode-symbol-render-text-emoji/
+        footnoteBackContent: "\u21A9\uFE0E"
+    }
+
+} satisfies lume_mdx.Options
