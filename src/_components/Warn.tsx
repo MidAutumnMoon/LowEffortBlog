a4814496c0ddc3ff2bbee555d751b0ef8aa8cea0
MidAutumnMoon <me@418.im>
2024-08-22 14:12:59 +0800

    add Warning component

---
+++ src/_components/Warn.tsx
@@ -0,0 +1,31 @@
+/**
+ * A Github styled warning blockquote
+ */
+
+import { ComponentChildren } from "preact"
+
+
+type Props = {
+    children: ComponentChildren,
+}
+
+export default function(
+    { children }: Props
+) {
+    return <blockquote
+        class="
+            not-prose
+            border-yellow-500 border-l-8 border
+            ps-3 pe-3 py-3
+        "
+    >
+
+        <p class="text-yellow-500 mb-3 flex gap-2">
+            <span class="icon-[mdi--warning-outline] w-6 h-6"></span>
+            <span class="">Warning</span>
+        </p>
+
+        <p>{ children }</p>
+
+    </blockquote>
+}
