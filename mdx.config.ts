052febccca80b08c7f78cfcb2e8553e3c4d4cdf3
MidAutumnMoon <me@418.im>
2024-08-22 22:08:39 +0800

    add mathjax support

--- mdx.config.ts
+++ mdx.config.ts
@@ -1,10 +1,17 @@
 import * as lume_mdx from "lume/plugins/mdx.ts"
 
+import RemarkMath from "remark-math"
+
 import RehypeSlug from "rehype-slug"
 import RehypeHeadingToc from "./_plugins/RehypeHeadingToc.ts"
+import RehypeMathjax from "rehype-mathjax"
 
 
 export default {
+
+    remarkPlugins: [
+        RemarkMath
+    ],
 
     rehypeOptions: {
         allowDangerousHtml: true,
@@ -15,6 +22,7 @@ export default {
     rehypePlugins: [
         RehypeSlug,
         RehypeHeadingToc,
+        RehypeMathjax,
     ]
 
 } satisfies lume_mdx.Options
