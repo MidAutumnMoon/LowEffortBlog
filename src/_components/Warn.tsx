8b25abfe1e33ba5188183c70d7bae52928bd4792
MidAutumnMoon <me@418.im>
2024-08-22 14:14:10 +0800

    raise the yellowness of Warning

--- src/_components/Warn.tsx
+++ src/_components/Warn.tsx
@@ -15,11 +15,12 @@ export default function(
     return <blockquote
         class="
             not-prose
-            border-yellow-500 border-l-8 border
+            border-yellow-400 border-l-8 border
             ps-3 pe-3 py-3
         "
     >
 
+        {/* yellow-400 will make the text harder to read */}
         <p class="text-yellow-500 mb-3 flex gap-2">
             <span class="icon-[mdi--warning-outline] w-6 h-6"></span>
             <span class="">Warning</span>
